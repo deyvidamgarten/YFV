617cc606a0180b64b90f659cdcf88467f9a1630e
Deyvid Emanuel Amgarten <drt46413@lqvtstap01.domhiae.einstein.br>
2019-06-18 13:08:22 -0300

    added new pipe for assembly

---
+++ execute_pipe_assembly_YFV.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+# Script to assembly YFV genomes with SPADES
+# Needs to be in a folder along with reference fasta file and raw fastq files from a sample
+
+#Create folder structure
+mkdir -p fastq reference mapped passedQC variants assembly human_mapped
+
+# Take sample file name
+FILE1=$(basename fastq/*R1* .fastq.gz)
+FILE2=$(basename fastq/*R2* .fastq.gz)
+# Generalize this
+FILE=$(basename fastq/*R1* _R1_001.fastq.gz)
+# Reference variable
+# Very important!!!!
+# Specify the name of your reference of choice inside the quotes!!!
+REFERENCE_HUMAN="/stgisilon_reference/basespace_hg19/Homo_sapiens/UCSC/hg19/FASTA_NC_012920_1/genome.fa"
+REFERENCE_YFV="reference/YFV_RJ104.fasta"
+
+# Align to human genome
+bwa mem -t 12 -M $REFERENCE_HUMAN passedQC/${SAMPLE1}.fq passedQC/${SAMPLE2}.fq | samtools view -b -q 30 -F 2304 -f 2  > human_mapped/${SAMPLE}_mapped_human.bam
+
+# Save unmapped reads
+samtools view -u -f 12 human_mapped/${SAMPLE}_mapped_human.bam > human_mapped/${SAMPLE}_mapped_human_unmapped.bam
+
+# Convert to FASTQ
+bamToFastq -i human_mapped/${SAMPLE}_mapped_human_unmapped.bam -fq human_mapped/${SAMPLE}_unmapped_human_R1.fastq -fq2 human_mapped/${SAMPLE}_unmapped_human_R2.fastq
+
+# Assembly
+spades.py -1 human_mapped/${SAMPLE}_unmapped_human_R1.fastq -2 human_mapped/${SAMPLE}_unmapped_human_R2.fastq --trusted-contigs $REFERENCE_YFV -t 12 -m 20 -o assembly/
+
+
+
+
+
+
+
+
